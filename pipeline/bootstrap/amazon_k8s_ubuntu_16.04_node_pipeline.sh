bbad69fefc473b6900b24c47c48d4471353e4ce9
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2018-07-12 22:20:47 +0200

    hostname param added

--- pipeline/bootstrap/amazon_k8s_ubuntu_16.04_node_pipeline.sh
+++ pipeline/bootstrap/amazon_k8s_ubuntu_16.04_node_pipeline.sh
@@ -22,6 +22,7 @@ cd ~
 
 TOKEN=$(cat /etc/kubicorn/cluster.json | jq -r '.values.itemMap.INJECTEDTOKEN')
 MASTER=$(cat /etc/kubicorn/cluster.json | jq -r '.values.itemMap.INJECTEDMASTER')
+HOSTNAME=$(hostname -f)
 
 sed -i -e 's|Environment="KUBELET_CADVISOR_ARGS=--cadvisor-port=0"|Environment="KUBELET_CADVISOR_ARGS=--cadvisor-port=0"\nEnvironment="KUBELET_EXTRA_ARGS=--cloud-provider=aws"|' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
@@ -33,7 +34,7 @@ export KUBECONFIG=/etc/kubernetes/kubelet.conf
 until kubectl get node | grep $(hostname -f)
 do
   kubeadm reset -f 
-  kubeadm join --discovery-token-unsafe-skip-ca-verification --token ${TOKEN} ${MASTER}
+  kubeadm join --discovery-token-unsafe-skip-ca-verification --node-name ${HOSTNAME} --token ${TOKEN} ${MASTER}
   echo "Waiting for Master to start up..."
   sleep 10
 done
